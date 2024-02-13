d8fdb257be2cf9a193e41a4933d0dfaa55638140
wariddon <Reaper_Destoyer@hotmail.com>
2024-02-13 16:53:24 +0700

    update digitalsignature

--- README.md
+++ README.md
@@ -12,3 +12,4 @@
 - [ตัวอย่างการเขียน asymmetric java](https://gregorycernera.medium.com/encrypting-and-decrypting-a-message-using-asymmetric-keys-with-java-explained-step-by-step-with-54fced36118a)
 - [เรียนรู้เกี่ยวกับ Infrastructure](https://www.jittagornp.me/blog/public-key-infrastructure/?series=pki)
 - [Symmetric Encryption VS Asymmetric Encryption](https://www.ert.co.th/symmetric-encryption-vs-asymmetric-encryption/)
+- [การทำลายเซ็นต์ดิจิตอล (Digital Signature) อย่างง่าย ด้วยภาษา Java](https://www.jittagornp.me/blog/java-digital-signature/?series=pki)
---
+++ jittagornp_example_DigitalSignatureExample.java
@@ -0,0 +1,64 @@
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.ECGenParameterSpec;
+
+public class jittagornp_example_DigitalSignatureExample {
+
+
+    private static KeyPair getKeyPair() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        final KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+        keyPairGenerator.initialize(new ECGenParameterSpec("secp256r1"), new SecureRandom());
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    private static Signature getSignature() throws NoSuchAlgorithmException {
+        return Signature.getInstance("SHA256withECDSA");
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, InvalidAlgorithmParameterException {
+
+        //1. Define Data
+        final String document = "Hello World";
+        final byte[] documentBytes = document.getBytes();
+        //====================================================
+
+        //2. Define KeyPair
+        final KeyPair keyPair = getKeyPair();
+        final PublicKey publicKey = keyPair.getPublic();
+        final PrivateKey privateKey = keyPair.getPrivate();
+
+        //3. Define Signature
+        final Signature signature = getSignature();
+        //====================================================
+
+        //4. Sign Data
+        signature.initSign(privateKey);
+        signature.update(documentBytes);
+        final byte[] signatureBytes = signature.sign();
+        //====================================================
+
+        //5. Verify Data
+        signature.initVerify(publicKey);
+
+        //Compare Data and Signature
+        signature.update(documentBytes);
+        final boolean isValid = signature.verify(signatureBytes);
+        if (isValid) {
+            System.out.println("Signature is valid");
+        } else {
+            System.out.println("Signature is not valid");
+        }
+
+    }
+
+
+}
